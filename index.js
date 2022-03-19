16e8133439f168d623393287613ff4309d731bf4
David Samuelson <dsamuelson89@gmail.com>
2022-03-19 13:06:04 -0600

    added require and started questions

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
-
+const { verify } = require('crypto');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the name of your project?(Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a project name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'userName',
+        message: 'What is your name?(Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'otherCont',
+        message: 'Were there any other contributors?',
+        default: false
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
